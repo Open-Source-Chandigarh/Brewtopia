2c42f4be181fed65e22d2aaa7517380add3350c8
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2023-12-31 12:19:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 //for cross site accessing
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://brewtopia.netlify.app");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
