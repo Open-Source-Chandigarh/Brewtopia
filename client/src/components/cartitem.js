9f021f9f7f66a11983b1e9595fbc915124fa78fd
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-01-01 18:16:32 +0530

    moved range from product to cart

--- client/src/components/cartitem.js
+++ client/src/components/cartitem.js
@@ -1,5 +1,6 @@
 import "../styles/App.css"
 import toast from "react-hot-toast";
+import { useState } from "react";
 import {MdDeleteOutline} from "react-icons/md"
 
 
@@ -7,6 +8,7 @@ export default function CartItem(props){
 
     //destructuring from the props object to obtain vars
     const { item, cart ,setCart } = props;
+    const [counter, setcounter] = useState(1);
 
     //to remove an item from the cart
     const removeitem = () => {
@@ -22,9 +24,31 @@ export default function CartItem(props){
             <div>
             <h4>{item.name} x {item.count}</h4>
             </div>
+            <div className="range">
+            <button
+              type="button"
+              onClick={() => {
+                if (counter === 1) {
+                } else {
+                  setcounter(counter - 1);
+                }
+              }}
+            >
+              -
+            </button>
+            <p>{counter}</p>
+            <button
+              type="button"
+              onClick={() => {
+                setcounter(counter + 1);
+              }}
+            >
+              +
+            </button>
+          </div>
             
             <div>
-                <p>₹{item.price * item.count}</p>
+                <p>₹{item.price * counter}</p>
                 <button type="button" onClick={removeitem}><MdDeleteOutline></MdDeleteOutline></button>
             </div>
         </div>
