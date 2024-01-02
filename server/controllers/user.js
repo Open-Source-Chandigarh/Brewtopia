e391267d2a85f75bbbbc9e244ec0933a0837b87d
Disha Modi <bhavnamodi802@gmail.com>
2024-01-02 20:56:12 +0530

    Added hashing of password using bcrypt

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,18 @@
+import bcryptjs from "bcryptjs"
 const UserModel = require("../models/Users");
 
 const createUser = async (req, res) => {
     try{
+        const reqBody = await req.json()
+
         //getting data from request body
         const user = req.body;
+        const {password} = reqBody;
 
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+        
+        user.password = hashedPassword;
         //creating new user in usermodel
         const newUser = new UserModel(user);
         
@@ -26,14 +34,14 @@ const getUser = async (req, res) => {
   
       //finding in database
       const Users = await UserModel.findOne({ username: username });
-  
+      const isValid = await bcryptjs.compare(password,Users.password);
       //no user return   ? error
       if(Users == null){
         return res.send("error bruh")
       }
   
       //user returned also equal ? giving data back so cookie can be made on client side
-      else if(username ===  Users.username && password === Users.password){
+      else if(username ===  Users.username && isValid){
         return res.json({
           name: Users.name,
           username: Users.username,
