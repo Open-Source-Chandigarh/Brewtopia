a64cbfe76d5ae2539570f17b4052a7da7eb97cbd
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-01-06 00:37:37 +0530

    add: signup/in on enter

--- client/src/login.js
+++ client/src/login.js
@@ -1,9 +1,9 @@
 import "./styles/login.css";
-import { useState} from "react";
+import { useState } from "react";
 import Axios from "axios";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
 export default function Login() {
   const [username, setusername] = useState("");
@@ -13,49 +13,51 @@ export default function Login() {
 
   //sending request to api for login
   const handleLogin = (e) => {
-
     //preventing default behaviour of refresh
     e.preventDefault();
 
-    if(!username){
+    if (!username) {
       return toast.error("Enter Username");
     }
 
-    if(!password){
+    if (!password) {
       return toast.error("Enter Password");
     }
 
-    //getting data from backend port 
+    //getting data from backend port
     const getUser = Axios.post("https://brewtopia.up.railway.app/getUser", {
       username: username,
       password: password,
-    })
-      .then((res) => {
-          // setting cookies to keep user logged in
-          if(res.data.username){
-            cookies.set('username', res.data.username , { sameSite: 'strict' });
-            cookies.set('name', res.data.name, { sameSite: 'strict' });
-            window.location.reload(false);
-          }else{
-            throw new Error();
-          }
-      })
+    }).then((res) => {
+      // setting cookies to keep user logged in
+      if (res.data.username) {
+        cookies.set("username", res.data.username, { sameSite: "strict" });
+        cookies.set("name", res.data.name, { sameSite: "strict" });
+        window.location.reload(false);
+      } else {
+        throw new Error();
+      }
+    });
 
-      toast.promise(getUser, {
-        loading: 'Logging in',
-        success: 'Logged in',
-        error: 'Username/password combination is wrong',
-      });
-
+    toast.promise(getUser, {
+      loading: "Logging in",
+      success: "Logged in",
+      error: "Username/password combination is wrong",
+    });
+  };
+  const handleEnterKey = (e) => {
+    if (e.key === "Enter") {
+      handleLogin(e);
+    }
   };
 
-  return (
+   return (
     <div className="login-wrapper">
       <div className="background">
         <div className="shape" />
         <div className="shape" />
       </div>
-      <form className="form">
+      <form className="form" onSubmit={handleLogin}>
         <h3>Login Here</h3>
         <input
           className="input"
@@ -63,6 +65,7 @@ export default function Login() {
           placeholder="Username"
           id="username"
           onChange={(e) => setusername(e.target.value)}
+          onKeyDown={handleEnterKey}
         />
         <input
           className="input"
@@ -70,11 +73,17 @@ export default function Login() {
           placeholder="Password"
           id="password"
           onChange={(e) => setpassword(e.target.value)}
+          onKeyDown={handleEnterKey}
+
         />
         <p className="redirect">
           Not a user ? <a href="/sign-up">sign up</a>
         </p>
-        <button type="button" onClick={handleLogin} className="button">
+        <button
+          type="button"
+          onClick={handleLogin}
+          className="button"
+        >
           Log In
         </button>
       </form>
