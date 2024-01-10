7660d6506f99fef1226c82b51b07bcabb51a898a
jayant-vashisth <jayant.relish@gmail.com>
2024-01-10 17:06:47 +0530

    API-rate-limiter added

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const {checkout , paymentverification} = require("../controllers/payment");
+const {paymentLimiter} = require("../middleware/rateLimiter");
 
 //used for checkout payment
 //all of functions for making razorpay order apply to instance
-router.post("/checkout",checkout)
+router.post("/checkout", paymentLimiter,checkout)
   
 //verifying payment if its really made or someone just made some credentials by themselves ??
 //this url is on callback_url in client side options in handlepayment so will be called from there
 //it will run after user has made payment
-router.post("/paymentverification", paymentverification)
+router.post("/paymentverification", paymentLimiter, paymentverification)
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {createUser , getUser} = require("../controllers/user.js")
+const {createUser , getUser} = require("../controllers/user.js");
+const {UserLimiter} = require("../middleware/rateLimiter.js");
 
-router.post("/createUser", createUser);
+router.post("/createUser", UserLimiter, createUser);
 
-router.post("/getUser", getUser);
+router.post("/getUser", UserLimiter, getUser);
 
 module.exports = router;
