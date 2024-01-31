498bf563f0569b66a0841d90ee271e14d67f23ce
Niharika0104 <124892559+Niharika0104@users.noreply.github.com>
2024-01-31 10:19:38 +0530

    Update user.js
    Remove sensitive logs from user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -58,12 +58,9 @@ const getUser = async (req, res) => {
     if (!user) {
       return res.json({ error: "User not found" });
     }
-    console.log(username+"username")
-    console.log(user.password+"expected password")
-    console.log(password+"entered password")
    
     const isValidhashedPassword =  bcryptjs.compare(password.trim(), user.password.trim());
-console.log(isValidhashedPassword+"valid password")
+
     // Incorrect password
     if (!isValidhashedPassword) {
       return res.json({ error: "Incorrect password" });
@@ -169,7 +166,7 @@ const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '5m' });
   try {
     const info = await transporter.sendMail(mailOptions);
    
-    console.log('Password reset email sent: ' + info.response);
+   
     res.json({ success: true });
   } catch (error) {
     console.error('Error sending password reset email:', error);
@@ -202,11 +199,10 @@ const resetPassword=async(req,res)=>{
 
 
     res.json({status:true})
-    console.log(`Password updated for user with username ${username}`);
+
   } else {
     res.json({status:false})
 
-    console.log(`User with username ${username} not found`);
   }
 }
 catch(error){
