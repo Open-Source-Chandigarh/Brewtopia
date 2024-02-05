d0dcb6ffb5f3433390f9a7f8bba055d6a1cf4fd5
Varinder Dhillon <98728916+Varinder-Dhillon0@users.noreply.github.com>
2024-02-05 09:40:13 +0530

    Revert "feat:Added forgot password using email based approach and also employed jwt tokens and cookies for enhanced security"

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {createUser , getUser,forgotPassword,sendEmail,resetPassword,markTokenAsConsumed} = require("../controllers/user.js");
+const {createUser , getUser} = require("../controllers/user.js");
 const {UserLimiter} = require("../middleware/rateLimiter.js");
 
 router.post("/createUser", UserLimiter, createUser);
 
 router.post("/getUser", UserLimiter, getUser);
-
-router.post("/forgetpassword", UserLimiter, forgotPassword);
-
-router.post("/sendEmail", UserLimiter, sendEmail);
-
-router.post("/resetPassword", UserLimiter, resetPassword);
-router.post("/tokenconsumed", UserLimiter, markTokenAsConsumed);
-
 
 module.exports = router;
